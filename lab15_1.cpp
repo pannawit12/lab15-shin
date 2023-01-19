fb92e1dd1f801518de61791f2c5fb0a37f9d5c55
pannawit12 <110965510+pannawit12@users.noreply.github.com>
2023-01-19 17:29:26 +0700

    Update lab15_1.cpp
    finished lab15-1

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,25 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    if(N>1){
+        insertionSort(d,N-1);
+        T x[N];
+        for(int i = 0; i < N; i++) x[i]=d[i];
+        int i=0;
+        while (i<N){
+            if(d[N-1]>=d[i]){
+                d[i]=d[N-1];
+                break;
+            }
+            i++;
+        }
+        for(int j=i+1;j<N;j++){
+            d[j]=x[j-1];
+        }
+        cout<<"Pass "<<N-1<<":";
+        for(i = 0; i < 10; i++) cout<<d[i]<<' ';
+        cout<<endl;
+    }
 }
 
 int main(){
